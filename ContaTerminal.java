3c56813784bbdafb37337cd6a0e610ba8e8a3620
Bruna Araujo <164442141+brunaraujo1@users.noreply.github.com>
2024-05-09 19:08:37 -0300

    Update ContaTerminal.java
    Adicionar interação do usuário com a classe Scanner

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        // Simulação de dados pré-preenchidos para teste
-        String agencia = "067-8";
-        int numero = 1021;
-        String nomeCliente = "MARIO ANDRADE";
-        double saldo = 237.48;
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar e ler os dados da conta bancária
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta:");
+        double saldo = scanner.nextDouble();
 
         // Exibir mensagem com os dados da conta bancária
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco,");
         System.out.println("sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
